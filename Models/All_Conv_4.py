e07acfbdc6b8267cba5a5714373c90cc07918eea
chris89culley <iamchris88@live.co.uk>
2020-12-09 22:27:01 +0000

    updating all conv

--- Models/All_Conv_4.py
+++ Models/All_Conv_4.py
@@ -1,21 +1,19 @@
 import math
 import seaborn as sns
 import copy
-import torch 
+import torch
 import numpy as np
 from Models.Weight_Fix_Base import Weight_Fix_Base
 import torch.nn as nn
-from groupy.gconv.pytorch_gconv.splitgconv2d import P4ConvZ2, P4ConvP4
-from groupy.gconv.pytorch_gconv.pooling import plane_group_spatial_max_pooling
 import torch.nn.functional as F
-import pytorch_lightning as pl 
+import pytorch_lightning as pl
 import torch.nn.utils.prune as prune
 from pytorch_lightning.metrics import functional as FM
 import matplotlib.pyplot as plt
 
 
 class All_Conv_4(Weight_Fix_Base):
-    def __init__(self):
+    def __init__(self, max_epochs):
         super(All_Conv_4, self).__init__()
         dim_in = [3,32,32]
         self.conv1 = nn.Conv2d(dim_in[0], 64, kernel_size=3, stride = 1)
@@ -31,20 +29,20 @@ class All_Conv_4(Weight_Fix_Base):
         self.pool = nn.MaxPool2d(kernel_size=2)
         self.forward_order = [self.conv1, F.relu, self.conv2, F.relu, self.pool, self.conv3, F.relu, self.conv4 ,F.relu, self.pool]
         self.lr = 3e-4
-        
+        self.max_epochs = max_epochs
+
     def set_optim(self):
            self.optim = torch.optim.Adam(self.parameters(), lr = self.lr)
-           self.scheduler = torch.optim.lr_scheduler.StepLR(
-                 self.optim, 
-                 step_size=50, 
-                 gamma=0.1) 
-                 
+           self.scheduler =   torch.optim.lr_scheduler.OneCycleLR(self.optim, max_lr=self.lr,
+                                                                         steps_per_epoch=45000//256,
+                                                                         epochs=self.max_epochs+1)
+
+
     def forward(self, x):
         for f in self.forward_order:
-            x = f(x) 
+            x = f(x)
         x = torch.flatten(x,1)
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.final(x)
         return F.log_softmax(x, dim = 1)
-
