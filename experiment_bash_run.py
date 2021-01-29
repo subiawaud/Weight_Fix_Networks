6945c792b75d7e5c32e8e118ddf3ce40acc41705
chris89culley <iamchris88@live.co.uk>
2021-01-29 16:28:11 +0000

    updated exp bas

--- experiment_bash_run.py
+++ experiment_bash_run.py
@@ -2,19 +2,16 @@ import os
 import itertools
 
 percentages = ["0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.95 0.975 0.99 0.995 0.999 1.0"]
-limits = ["0.001 0.0005 0.00025 0.002"]
-changes = ["0.0001 0.000025 0.0002 0.00005"]
-first_epochs = [40]
-rest_epochs = [20, 5, 10]
-bits = [32]
-script_test =f'python all_conv_experiments.py --limits_to_try 0.002 0.0025 --change_to_try 0.0001 0.005 --percentages {percentages} --first_epoch 1 --fixing_epochs 1 --bits 16 --name "testing_argparse"'
-ts = [1, 0.75, 0.5]
-gammas = [0.5, 0.25, 0.1, 0]
-encourage_plus_one_clusters = [True] #, False]
+distances_allowed = ["0.15", "0.1", "0.2", "0.25", "0.3", "0.35"]
+first_epochs = [0]
+rest_epochs = [15]
+bits = ['pow_2_add']
+gammas = [0.05, 0, 0.01, 0.025, 0.075, 0.1, 0.2, 0.4]
+model = 'allconv'
 
-params = list(itertools.product(*[percentages, limits, changes, first_epochs, rest_epochs, bits, ts, gammas, encourage_plus_one_clusters]))
+params = list(itertools.product(*[percentages, distances_allowed, first_epochs, rest_epochs, bits, gammas]))
 print(len(params))
 for param_set in params:
-         percentage, limit, c, f_e, r_e, b, t, gamma, epoc = param_set
-         script_test =f'--encourage_plus_one_cluster {epoc} --t {t} --gamma {gamma} --limits_to_try {limit} --change_to_try {c} --percentages {percentage} --first_epoch {f_e} --fixing_epochs {r_e} --bits {b} --name "testing_argparse" '
-         os.system('sbatch script.sh ' script_test)
+         percentage, da, f_e, r_e, b, gamma, = param_set
+         script_test =f'--distance_allowed {da} --regularistion_ratio {gamma} --percentages {percentage} --first_epoch {f_e} --fixing_epochs {r_e} --cluster_bit_fix {b} --name "exp_1" '
+         os.system('python pre_trained_model_experiments.py '+ script_test)
