01bb05914322e2a7eb025a25c106cdbe5210ff58
C.Culley <cc2u18@cyan51.cluster.local>
2021-02-01 16:18:46 +0000

    updated

--- experiment_bash_run.py
+++ experiment_bash_run.py
@@ -2,18 +2,18 @@ import os
 import itertools
 
 percentages = ["0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.95 0.975 0.99 0.995 0.999 1.0"]
-distances_allowed = ["0.005 0.0025 0.0075 0.01"]
+distances_allowed = ["0.005 0.0025 0.01 0.02 0.04 0.08"]
 first_epochs = [0]
 rest_epochs = [15]
 bits = ['pow_2_add']
-gammas = [0.05, 0, 0.025, 0.075, 0.1, 0.15,  0.2]
+gammas = [0.05, 0, 0.025, 0.1, 0.2, 0.0125]
 #gammas = [0]
 #model = 'allconv'
-zeros = [2**-x for x in range(6, 10)]
+zeros = [2**-x for x in range(6, 9)]
 model = 'resnet'
 
 params = list(itertools.product(*[percentages, distances_allowed, first_epochs, rest_epochs, bits, gammas, zeros]))
 for param_set in params:
          percentage, da, f_e, r_e, b, gamma,z = param_set
          script_test =f'--distance_allowed {da} --regularistion_ratio {gamma} --percentages {percentage} --first_epoch {f_e} --fixing_epochs {r_e} --cluster_bit_fix {b} --name "exp_1" --model {model} --zero_distance {z}'
-         os.system('python pre_trained_model_experiment.py'+ script_test)
+         os.system('sbatch script.sh '+ script_test)
